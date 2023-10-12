2baf24ee2b882563c1a758cdeadccf620dcd4b27
Krishna Biswakarma <krishnabiswakarmasc@gmail.com>
2023-10-13 00:46:18 +0530

    Initial Commit

---
+++ components/UI/index.js
@@ -0,0 +1,8 @@
+export { Button, buttonVariant } from "./Button";
+export { Input } from "./Input";
+export {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "./Tooltip";
