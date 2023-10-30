8ce922b94ad21551830ec2c2b902534789cfe248
Krishna Biswakarma <krishnabiswakarmasc@gmail.com>
2023-10-31 00:33:48 +0530

    Now the basic shorten service is functional.

--- apis/url.js
+++ apis/url.js
@@ -1,5 +1,15 @@
-import { postData } from "@/lib/services";
+import { getData, postData } from "@/lib/services";
+import toast from "react-hot-toast";
 
 export const handleGenerateURL = async (url) => {
-  return postData("/url", { url });
+  const urlRegex =
+    /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+  if (!urlRegex.test(url)) toast.error("Invalid URL");
+  await postData("/url", { url });
+  toast.success("URL shortened successfully");
+};
+
+export const showAllURLs = async (setAllURLs) => {
+  const response = await getData("/url");
+  setAllURLs(response.data);
 };
--- app/page.js
+++ app/page.js
@@ -1,20 +1,64 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { copyToClipboard } from "@/lib/utils";
 import { Toaster } from "react-hot-toast";
-import { useRouter } from "next/navigation";
 import { useMutation } from "@tanstack/react-query";
-import { handleGenerateURL } from "@/apis/url";
+import { handleGenerateURL, showAllURLs } from "@/apis/url";
 import { Button, Input } from "@/components/ui";
+import Link from "next/link";
+
+const Entry = ({ slug, originalUrl }) => {
+  const shortUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/${slug}`;
+  return (
+    <section className="flex items-center justify-between gap-5 rounded-lg border border-primary p-3">
+      <div className="flex-1">
+        <Link
+          href={originalUrl}
+          target="_blank"
+          className="text-slate-700 truncate w-96 text-md hover:underline"
+        >
+          {originalUrl || "Original URL"}
+        </Link>
+      </div>
+      <div className="flex items-center gap-5">
+        <div className="w-96">
+          <Link
+            href={shortUrl}
+            target="_blank"
+            className="text-slate-700 truncate text-md hover:underline"
+          >
+            {shortUrl || "Short URL"}
+          </Link>
+        </div>
+        <Button
+          variant="ghost"
+          onClick={() => copyToClipboard(shortUrl, "Copied the short URL")}
+        >
+          Copy
+        </Button>
+      </div>
+    </section>
+  );
+};
 
 export default function Home() {
   //Variables
-  const router = useRouter();
   const [url, setUrl] = useState("");
+  const [allURLs, setAllURLs] = useState([]);
 
   //APIs
-  const { isLoading, mutate } = useMutation(() => handleGenerateURL(url));
+  const { isLoading, mutate } = useMutation(() => handleGenerateURL(url), {
+    onSuccess: () => {
+      showAllURLs(setAllURLs);
+      setUrl("");
+    },
+  });
+
+  //UseEffects
+  useEffect(() => {
+    showAllURLs(setAllURLs);
+  }, []);
 
   //Functions
   const handleGenerateShortLink = (event) => {
@@ -29,7 +73,7 @@ export default function Home() {
       }}
       className="flex flex-col items-center justify-center gap-5 h-screen"
     >
-      <div className="w-2/3 flex flex-col gap-10">
+      <div className="lg:w-2/3 w-full lg:px-0 px-10 flex flex-col gap-10 h-[70%] overflow-hidden">
         {/* Header */}
         <section className="flex flex-col">
           <h1 className="text-5xl text-primary">Free URL Shortener</h1>
@@ -37,7 +81,6 @@ export default function Home() {
             Create short & memorable links in seconds.
           </p>
         </section>
-
         {/* Input Form */}
         <form
           onSubmit={handleGenerateShortLink}
@@ -51,26 +94,20 @@ export default function Home() {
             onChange={(e) => setUrl(e.target.value)}
           />
           <Button type="submit" disabled={isLoading}>
-            Generate
+            {isLoading ? "Generating..." : "Generate"}
           </Button>
         </form>
 
         {/* Shortened Link */}
-        <section className="flex items-center justify-between gap-5 rounded-lg border border-primary p-3">
-          <p className="text-slate-700 flex-1 truncate">Actual Link</p>
-          <div className="flex items-center gap-5">
-            <Button
-              variant="link"
-              onClick={() => router.push("/")}
-              className="text-slate-700 truncate w-96"
-            >
-              https://example.com
-            </Button>
-            <Button variant="ghost" onClick={() => copyToClipboard(url)}>
-              Copy
-            </Button>
-          </div>
-        </section>
+        <div className="flex flex-col overflow-scroll gap-5">
+          {allURLs?.map((entry) => (
+            <Entry
+              key={entry._id}
+              originalUrl={entry.redirectURL}
+              slug={entry.slug}
+            />
+          ))}
+        </div>
       </div>
       <Toaster />
     </div>
