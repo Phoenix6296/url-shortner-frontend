a7e1d3e71631b09451432aedd0892ff7755fc32f
Krishna Biswakarma <krishnabiswakarmasc@gmail.com>
2023-11-03 00:10:50 +0530

    Added utility functions for extracting first name and to capitalize the first letter of the word

--- apis/auth.js
+++ apis/auth.js
@@ -1,11 +1,12 @@
 import { postData } from "@/lib/services";
+import { firstName } from "@/lib/utils";
 import toast from "react-hot-toast";
 
 export const handleSignup = async ({ name, email, password, onClose }) => {
   try {
     const response = await postData("/auth/signup", { name, email, password });
     if (response.status === 201) {
-      toast.success(`Welcome ${name.split(" ")[0]}!`);
+      toast.success(`Welcome ${firstName(name)}!`);
       onClose();
     } else {
       throw new Error(response.data.message);
@@ -20,7 +21,7 @@ export const handleLogin = async ({ email, password, onClose }) => {
     const response = await postData("/auth/login", { email, password });
     if (response.status === 200) {
       const name = response.data.name;
-      toast.success(`Welcome ${name.split(" ")[0]}!`);
+      toast.success(`Welcome ${firstName(name)}!`);
       onClose();
     } else {
       throw new Error(response.data.message);
